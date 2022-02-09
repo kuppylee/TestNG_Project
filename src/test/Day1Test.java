920239248a71c01b26c936fb03fc8315dbe3dac1
Mobolaji <bjkuppy@gmail.com>
2022-02-09 09:40:30 +0100

    installed the necessary jar files

---
+++ src/test/Day1Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day1Test {
+//    public static void main(String[] args){
+//        demo();
+//    }
+    @Test
+    public static void demo(){
+        System.out.println("hello");
+    }
+
+}
