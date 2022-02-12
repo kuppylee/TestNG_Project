2d84bca077f9df148603f0a0dd3ceea78811a85f
Mobolaji <bjkuppy@gmail.com>
2022-02-12 11:55:18 +0100

    prioritizing the testcases with TestNG

---
+++ src/test/Day3.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day3 {
+
+    @Test
+    public void webLogiCarLoan(){
+        // selenium
+        System.out.println("web login car ");
+    }
+
+    @Test
+    public void mobileLoginCarLoan(){
+        // Appium code
+        System.out.println("mobile login car ");
+
+    }
+
+    @Test
+    public void loginAPICarLoan(){
+        // Rest API automation
+        System.out.println("login API car ");
+    }
+
+}
---
+++ src/test/Day4.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day4 {
+    @Test
+    public void webLoginHomeLoan(){
+        // selenium
+        System.out.println("web login home ");
+    }
+
+    @Test
+    public void mobileLoginHomeLoan(){
+        // Appium code
+        System.out.println("mobile login home ");
+
+    }
+
+    @Test
+    public void loginAPIHomeLoan(){
+        // Rest API automation
+        System.out.println("login API home");
+    }
+}
